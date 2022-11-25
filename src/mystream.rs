8b154ec095ed07878aa3f49dcd016e051276bcc8
isnlan <snlan@live.cn>
2022-11-25 13:59:01 +0800

    update

--- src/mystream.rs
+++ src/mystream.rs
@@ -132,7 +132,7 @@ mod tests {
     use super::*;
 
     #[tokio::test]
-    async fn it_works() -> anyhow::Result<()> {
+    async fn test_reader() -> anyhow::Result<()> {
         let (bus_tx, _bus_rx) = sync::mpsc::channel(10);
 
         let (mut a, b) = new(1, "tcp", "127.0.0.1:8888", bus_tx);
@@ -170,70 +170,20 @@ mod tests {
         Ok(())
     }
 
-    #[test]
-    fn test_reader() {
-        let mut v = [0u8; 10];
-        let mut buf = tokio::io::ReadBuf::new(&mut v);
-        println!("remaing {}", buf.remaining());
-        buf.put_slice("ping".as_bytes());
-
-        println!("remaing {}", buf.remaining());
-        // println!(" --- >{:?}", v);
-        let mut b1 = buf.filled();
-        // b1.(&b"fsf"[..]);
-        println!("0 --- >{:?}", b1);
-
-        println!("1 --- >{:?}", v);
-        // println!("1 --- >{:?}", b1);
-    }
-
     #[tokio::test]
-    async fn test_readr_trait() -> anyhow::Result<()> {
-        let (mut a, mut b) = tokio::io::duplex(8);
-        let l1 = a.write(b"pingpingping").await?;
+    async fn test_writer() -> anyhow::Result<()> {
+        let (bus_tx, mut bus_rx) = sync::mpsc::channel(10);
 
-        let mut v = [0u8; 12];
-        let mut buf = tokio::io::ReadBuf::new(&mut v);
-        // buf.put_slice("hello".as_bytes());
-
-        let b1 =
-            unsafe { &mut *(buf.unfilled_mut() as *mut [std::mem::MaybeUninit<u8>] as *mut [u8]) };
-
-        let ret = b.read(b1).await?;
-        unsafe { buf.assume_init(ret) };
-        buf.advance(ret);
-
-        println!("========== {:?}", v);
-
-        Ok(())
-    }
-
-    #[tokio::test]
-    async fn test_readr_trait1() -> anyhow::Result<()> {
-        let (mut a, mut b) = tokio::io::duplex(8);
-
+        let (mut a, b) = new(1, "tcp", "127.0.0.1:8888", bus_tx);
 
         task::spawn(async move {
-            let l1 = a.write_all(b"pingpingping").await.unwrap();
+            while let Some(msg) = bus_rx.recv().await {
+                println!("<<-- {:?}", msg);
+            }
         });
 
-        println!("-----");
-        for i in 0..3 {
-            let mut v = [0u8; 10];
-            let mut buf = tokio::io::ReadBuf::new(&mut v);
-            // buf.put_slice("hello".as_bytes());
-
-            let b1 = unsafe {
-                &mut *(buf.unfilled_mut() as *mut [std::mem::MaybeUninit<u8>] as *mut [u8])
-            };
-
-            let ret = b.read_exact(b1).await?;
-            unsafe { buf.assume_init(ret) };
-            buf.advance(ret);
-
-            tokio::time::sleep(Duration::from_secs(1)).await;
-            println!("========== {:?}", v);
-        }
+        a.write(b"hello").await?;
+        tokio::time::sleep(Duration::from_secs(1)).await;
 
         Ok(())
     }
